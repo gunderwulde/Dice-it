896eea9c0ca0930d6fab93b7e51899abee110da2
Glitch (dice-gl) <none>
2018-03-08 15:29:05 +0000

    🏣🌖 Checkpoint Texture.js:709155/91 meshLoader.js:709155/274 basegl.js:709155/11

--- Texture.js
+++ Texture.js
@@ -7,8 +7,9 @@ Texture.prototype.Load = function(gl, url, onLoad){
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]) );
   const image = new Image();
   image.setAttribute('crossorigin', 'anonymous');
+  var self = this;
   image.onload = function() {
-    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.bindTexture(gl.TEXTURE_2D, self.texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
        gl.generateMipmap(gl.TEXTURE_2D);
@@ -17,15 +18,15 @@ Texture.prototype.Load = function(gl, url, onLoad){
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     }
-    onLoad();
+    if(onLoad!=undefined) onLoad();
   };
   image.src = url;
 }
 
-Texture.protortype.Set = function(gl){
+Texture.prototype.Set = function(gl, programInfo){
   gl.activeTexture(gl.TEXTURE0);
   // Bind the texture to texture unit 0
-  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.bindTexture(gl.TEXTURE_2D, this.texture);
   // Tell the shader we bound the texture to texture unit 0
   gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 };
--- meshLoader.js
+++ meshLoader.js
@@ -1,12 +1,15 @@
 function Mesh() {
   this.submeshes = [];
+  this.modelMatrix = new Matrix4();
+    const normalMatrix = new Matrix4();
+
 }
 
 Mesh.prototype.LoadMesh = function(gl, url, onLoad ){
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'arraybuffer';
-  xhr.mesh = this;
+  var self = this;
   xhr.onload = function(e){
     if (this.status == 200) {
       var view = new DataView( this.response );
@@ -36,27 +39,27 @@ Mesh.prototype.LoadMesh = function(gl, url, onLoad ){
       
       for (var j = 0; j < subMeshCount; j++) {
         var indexCount = view.getUint16(idx,true); idx+=2;
-        this.mesh.submeshes.push( { offset:indexOffset, count: indexCount } );
+        self.submeshes.push( { offset:indexOffset, count: indexCount } );
         for (var i = 0; i < indexCount; i++) {
           indices.push( view.getUint16(idx,true) ); idx+=2;
         }
         indexOffset += indexCount;
       }
       
-      this.mesh.positionBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.mesh.positionBuffer);
+      self.positionBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, self.positionBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
       
-      this.mesh.normalBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.mesh.normalBuffer);
+      self.normalBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, self.normalBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
 
-      this.mesh.textureCoordBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.mesh.textureCoordBuffer);      
+      self.textureCoordBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, self.textureCoordBuffer);      
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW );
       
-      this.mesh.indexBuffer = gl.createBuffer();
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.mesh.indexBuffer);
+      self.indexBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.indexBuffer);
       gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);      
       onLoad();
     }        
@@ -81,8 +84,11 @@ Mesh.prototype.Draw = function(gl, programInfo, texture){
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
     // Tell WebGL to use our program when drawing
     gl.useProgram(programInfo.program);
+// Falta poner las matrices de este objeto.  
   
-     
+  
+    texture.Set(gl,programInfo);
+    
     for( var i=0;i<this.submeshes.length;++i)
       gl.drawElements(gl.TRIANGLES, this.submeshes[i].count, gl.UNSIGNED_SHORT, this.submeshes[i].offset*2);
   
