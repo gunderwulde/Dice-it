bdf57e76c2714ed90842f481a3901737b7cd1ed0
Glitch (dice-gl) <none>
2018-03-08 14:56:40 +0000

    📺🚩 Checkpoint Texture.js:709155/125 basegl.js:709155/24 Matrix4.js:709155/4

--- Texture.js
+++ Texture.js
@@ -1,2 +1,24 @@
 function Texture() {
 }
+
+Texture.prototype.Load = function(gl, url, onLoad){
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]) );
+  const image = new Image();
+  image.setAttribute('crossorigin', 'anonymous');
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+    onLoad();
+  };
+  image.src = url;
+return texture;
+}
