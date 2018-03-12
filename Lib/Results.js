b77c535d09f73a94ac7406ad596cc562935fc96c
Glitch (dice-gl) <none>
2018-03-12 12:45:37 +0000

    🛫🌧 Checkpoint Lib/Results.js:709155/1380

--- Lib/Results.js
+++ Lib/Results.js
@@ -1,10 +1,6 @@
-
-
 function Results() {
   this.name="Anim";
-  this.currentIndex = 0;
-  this.time = 0;
-  this.play = false;
+  this.current = null;
 }
 
 Results.prototype.Load = function(url, onLoad ){
@@ -18,35 +14,62 @@ Results.prototype.Load = function(url, onLoad ){
     if (this.status == 200) {
       var view = new DataView( this.response );
       var idx=0;
-      self.results = view.getUint16(idx,true); idx+=2;
-      self.frames = view.getUint16(idx,true); idx+=2;
-      
-      self.dicePosition =[];
-      self.diceRotation =[];
-      self.cameraPosition =[];
-      self.cameraRotation =[];
-      
-      for(var i=0;i<self.frames*3;i++){ self.dicePosition.push(view.getFloat32(idx,true));idx+=4;}
-      for(var i=0;i<self.frames*3;i++){ self.diceRotation.push(view.getFloat32(idx,true));idx+=4;}
-      for(var i=0;i<self.frames*3;i++){ self.cameraPosition.push(view.getFloat32(idx,true));idx+=4;}
-      for(var i=0;i<self.frames*3;i++){ self.cameraRotation.push(view.getFloat32(idx,true));idx+=4;}
+      self.count = view.getUint16(idx,true); idx+=2;
+      self.result = [];
+      for(var i=0;i<self.count;i++){
+        self.result[i] = new Result();
+        idx = self.result[i].Load(view,idx);
+      }
       
       if(onLoad!=undefined) onLoad(self);
       mainScene.Loader.Pop(self);
-      console.log("Loaded");
     }
   }
   xhr.send();
 }
 
 Results.prototype.Update = function(deltaTime, dice, camera ){
-  if(this.play){    
+  if(this.current!=null)
+    if( !this.current.Update(deltaTime, dice, camera) )
+      this.current=null;
+}
+
+Results.prototype.Throw = function(value){
+  this.current = this.result[0];
+}
+
+
+function Result() {
+  this.currentIndex = 0;
+  this.time = 0;
+}
+
+Result.prototype.Reset = function(){
+  this.currentIndex = 0;
+  this.time = 0;
+}
+
+Result.prototype.Load = function(view,idx){
+    this.frames = view.getUint16(idx,true); idx+=2;
+
+    this.dicePosition =[];
+    this.diceRotation =[];
+    this.cameraPosition =[];
+    this.cameraRotation =[];      
+    for(var i=0;i<self.frames*3;i++){ self.dicePosition.push(view.getFloat32(idx,true));idx+=4;}
+    for(var i=0;i<self.frames*3;i++){ self.diceRotation.push(view.getFloat32(idx,true));idx+=4;}
+    for(var i=0;i<self.frames*3;i++){ self.cameraPosition.push(view.getFloat32(idx,true));idx+=4;}
+    for(var i=0;i<self.frames*3;i++){ self.cameraRotation.push(view.getFloat32(idx,true));idx+=4;}
+  
+  return idx;
+}
+
+Result.prototype.Update = function(deltaTime, dice, camera){
     this.time+=deltaTime;
     if(this.time > 0.03) {
-      this.time=0.00;
+      this.time=0.00;      
       if(this.currentIndex>=this.frames-1) {
-        this.play=false;
-        return;
+        return false;
       }
       else
         this.currentIndex++;
@@ -57,9 +80,5 @@ Results.prototype.Update = function(deltaTime, dice, camera ){
       dice.Position( this.dicePosition[idx+0], this.dicePosition[idx+1], this.dicePosition[idx+2] );
       dice.Rotation( this.diceRotation[idx+0], this.diceRotation[idx+1], this.diceRotation[idx+2] );
     }
-  }
-}
-
-Results.prototype.Throw = function(value){
-  this.play = true;
+  return true;
 }
