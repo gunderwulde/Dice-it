81ae3c1ba504f0d29187463a28dd308a498cef48
Glitch (dice-gl) <none>
2018-03-13 11:23:11 +0000

    Exporting to gunderwulde/dice-gl

--- Lib/Assets.js
+++ Lib/Assets.js
@@ -1,33 +1,32 @@
 function Assets(){
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', ".glitch-assets", true);
-//  xhr.responseType = 'arraybuffer';
- 
-  var self = this;
-  
-  self.elements=[];
+  if(window.location.href =="https://dice-gl.glitch.me/"){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', ".glitch-assets", true);
+    var self = this;  
+    self.elements=[];
+    mainScene.Loader.Push(self);  
+    xhr.onload = function(e){
+      if (this.status == 200) {
 
-  mainScene.Loader.Push(self);  
-  xhr.onload = function(e){
-    if (this.status == 200) {
-      
-      var str = this.response.split("\n").join(",\n");
-      var elems = JSON.parse('{"elems":[' + str + "{}]}").elems;
-      for( var i=0;i<elems.length;++i){
-        
-        self.elements[elems[i].name]  = elems[i].url;
+        var str = this.response.split("\n").join(",\n");
+        var elems = JSON.parse('{"elems":[' + str + "{}]}").elems;
+        for( var i=0;i<elems.length;++i){
+
+          self.elements[elems[i].name]  = elems[i].url;
+        }
+
+        self.then(self);      
+        mainScene.Loader.Pop(self);
       }
-      
-      self.then(self);      
-      mainScene.Loader.Pop(self);
     }
-  }
-  xhr.send();
+    xhr.send();
+  }else
+    self.then(self);      
 }
 
 Assets.prototype.getURL = function(name){
   if(window.location.href =="https://dice-gl.glitch.me/")
     return this.elements[name];
   
-return "https://gunderwulde.github.io/dice-gl/Assets/"+name;
+return "Assets/"+name;
 }
