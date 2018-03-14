41dbaedcfc2848b4fb44e872e800d4640850e1d4
Glitch (dice-gl) <none>
2018-03-14 09:25:25 +0000

    🚝🏟 Checkpoint Lib/Assets.js:709155/196

--- Lib/Assets.js
+++ Lib/Assets.js
@@ -8,8 +8,13 @@ function Assets(){
     if (this.status == 200) {
       var str = this.response.split("\n").join(",\n");
       var elems = JSON.parse('{"elems":[' + str + "{}]}").elems;
-      for( var i=0;i<elems.length;++i)
-        self.elements[elems[i].name]  = elems[i].url;
+      for( var i=0;i<elems.length;++i){
+        if(elems[i].deleted!=undefined){
+          console.log( ">> "+ elems[i].date );
+          self.elements[elems[i].name]  = elems[i].url;
+        }
+      }
+      console.log( ">> "+ self.elements.length );
     }
     self.then(self);
     mainScene.Loader.Pop(self);
