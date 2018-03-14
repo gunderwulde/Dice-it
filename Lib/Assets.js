646b981d941d53745e70ade140a23f5ce54bbbb0
Glitch (dice-gl) <none>
2018-03-14 14:48:28 +0000

    ✊🌠 Checkpoint ./Lib/Assets.js:709155/2150

--- Lib/Assets.js
+++ Lib/Assets.js
@@ -1,29 +1,37 @@
 function Assets(){
+  this.onGlitch = window.location.href.indexOf("glitch.me") !== -1;
   var xhr = new XMLHttpRequest();
   xhr.open('GET', ".glitch-assets", true);
   var self = this;  
   self.elements=[];
-  mainScene.Loader.Push(self);  
+  if (typeof mainScene !== 'undefined') mainScene.Loader.Push(self);  
+  
   xhr.onload = function(e){
     if (this.status == 200) {
       var str = this.response.split("\n").join(",\n");
       var elems = JSON.parse('{"elems":[' + str + "{}]}").elems;
       for( var i=0;i<elems.length;++i){
-        if(elems[i].deleted!=undefined){
-          console.log( ">> "+ elems[i].date );
-          self.elements[elems[i].name]  = elems[i].url;
+        if(("date" in elems[i])){
+          if(self.elements[elems[i].name]!=null){
+            if(elems[i].date > self.elements[elems[i].name].date){
+               self.elements[elems[i].name]  = elems[i];
+            }
+          }
+          else
+            self.elements[elems[i].name]  = elems[i];
+          self.elements[elems[i].name].url = self.elements[elems[i].name].url+"?"+Date.parse(self.elements[elems[i].name].date);
         }
       }
-      console.log( ">> "+ self.elements.length );
     }
-    self.then(self);
-    mainScene.Loader.Pop(self);
+    Assets.instance = self;    
+    if("then" in self) self.then(self);
+    if (typeof mainScene !== 'undefined') mainScene.Loader.Pop(self);
   }
   xhr.send();
 }
 
 Assets.prototype.getURL = function(name){
-  if(window.location.href =="https://dice-gl.glitch.me/")
-    return this.elements[name];  
+  if(this.onGlitch)
+    return this.elements[name].url;  
 return "Assets/"+name;
 }
